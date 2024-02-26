40369bd6047b10e0be613437689877a0e0032caa
Shubham <sharmashubham2546486@gmail.com>
2024-02-26 10:31:58 +0530

    azure ad task completed

---
+++ azure ad task/AzureAdJavaConnect/src/main/java/com/azure/ad/java/connect/AzureAdJavaConnectApplication.java
@@ -0,0 +1,13 @@
+package com.azure.ad.java.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAdJavaConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureAdJavaConnectApplication.class, args);
+	}
+
+}
---
+++ azure ad task/AzureAdJavaConnect/src/test/java/com/azure/ad/java/connect/AzureAdJavaConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.ad.java.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureAdJavaConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
